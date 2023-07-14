477787a4dc69ab82b9e9fa353e07e69b25c98e94
Mike Emery <mike@mytanda.io>
2023-07-14 14:56:33 -0400

    Adding Sandbox support for public token and processor token

---
+++ lib/plaid/sandbox.ex
@@ -0,0 +1,63 @@
+defmodule Plaid.Sandbox do
+
+  alias Plaid.Client.Request
+  alias Plaid.Client
+
+  defmodule PublicToken do
+    @derive Jason.Encoder
+    defstruct public_token: nil,
+              request_id: nil
+
+    def create_public_token(params, config \\ %{}) do
+      request_operation("sandbox/public_token/create", params, config)
+    end
+
+    defp request_operation(endpoint, params, config) do
+      c = config[:client] || Plaid
+
+      Request
+      |> struct(method: :post, endpoint: endpoint, body: params)
+      |> Request.add_metadata(config)
+      |> c.send_request(Client.new(config))
+      |> c.handle_response(&map_link(&1))
+    end
+
+
+    defp map_link(body) do
+      Poison.Decode.transform(
+        body,
+        %{
+          as: %Plaid.Sandbox.PublicToken{}
+        }
+      )
+    end
+  end
+
+  defmodule ProcessorToken do
+    defstruct processor_token: nil,
+              request_id: nil
+
+    def create_processor_token(params, config \\ %{}) do
+      request_operation("sandbox/public_token/create", params, config)
+    end
+
+    defp request_operation(endpoint, params, config) do
+      c = config[:client] || Plaid
+
+      Request
+      |> struct(method: :post, endpoint: endpoint, body: params)
+      |> Request.add_metadata(config)
+      |> c.send_request(Client.new(config))
+      |> c.handle_response(&map_link(&1))
+    end
+
+    defp map_link(body) do
+      Poison.Decode.transform(
+        body,
+        %{
+          as: %Plaid.Sandbox.ProcessorToken{}
+        }
+      )
+    end
+  end
+end
